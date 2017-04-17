d8d9acb05ab8b705e52967d5948e0d87a116fcea
LYFhub <liyf963@163.com>
2017-04-17 17:37:19 +0800

    test es6 grammar

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 class App extends Component {
   render() {
+    let word = "world";
     return (
       <div className="App">
         <div className="App-header">
@@ -12,6 +15,9 @@ class App extends Component {
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p className="App-intro">
+          Hello {word}
         </p>
       </div>
     );
